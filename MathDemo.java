54afaf6aa2ec0656476915c87324bdd7ed30bdf5
jrp497 <jrp497@nyu.edu>
2016-04-26 01:01:48 -0400

    Android Demos

--- MathDemo.java
+++
@@ -1,5 +0,0 @@
-class MathDemo{
-	public static void main(String args[]){
-		System.out.println(Math.round(Math.random()*10000));
-		}
-	}
--- Packages/ClassA/A.java
+++
@@ -1,9 +0,0 @@
-package ClassA;
-
-public class A{
-	public void display(){
-		System.out.println("Class A");
-		C c1 = new C();
-		c1.displayc();
-		}
-	}
